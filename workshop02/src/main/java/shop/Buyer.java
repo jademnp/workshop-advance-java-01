ed04bb7385d942818e9bf1d161ee0a5d539b7d48
KBANKNET\jade.m <jade.m@kbtg.tech>
2021-02-09 11:06:20 +0700

    fixed

--- workshop02/src/main/java/shop/Buyer.java
+++ workshop02/src/main/java/shop/Buyer.java
@@ -1,30 +1,106 @@
 package shop;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Buyer {
     private String firstName;
     private String lastName;
 
-    public void requestToBuy(){
-        //1. create basket
+    public void requestToBuy() {
+        // 1. Create basket
         Basket basket = new Basket();
-        //2. add book to basket
-        basket.addBook(null);
-        //3. checkout
-        //TODO
+        // 2. Add book to basket
+        Book book1 = new Book("Potter 1", 8);
+        Book book2 = new Book("Potter 2", 8);
+        Book book3 = new BookBuilder().chooseBook("Potter 3").build();
+        basket.addBook(book1);
+        basket.addBook(book2);
+        basket.addBook(book3);
+        // 3. Checkout
+        // TODO :: checkout process
+        Checkout checkout = new Checkout();
+        checkout.process(basket);
+
+        // Check netPrice = 24, discountPrice 24 - 10%
     }
 }
-class Basket{
-    List<Book> books;
 
-    public void addBook(Book book){
+class Checkout {
+    public void process(Basket basket) {
+        int netPrice = PriceCalculator.get(basket);
+        int maxDiscount = DiscountCalculator.get(basket);
+        int totalPrice = netPrice - maxDiscount;
+        // TODO
+    }
+}
+
+class PriceCalculator {
+
+    public static int get(Basket basket) {
+        // Logic
+        return 0;
+    }
+}
+
+class DiscountCalculator {
+
+    public static int get(Basket basket) {
+        // Logic
+        return 0;
+    }
+}
+
+class Basket {
+    List<Book> books = new ArrayList<Book>();
+    private int netPrice;
+    private int discountPrice;
+
+    public void addBook(Book book) {
         books.add(book);
     }
 
+    public int getNetPrice() {
+        return netPrice;
+    }
+
+    public void setNetPrice(int netPrice) {
+        this.netPrice = netPrice;
+    }
+
+    public int getDiscountPrice() {
+        return discountPrice;
+    }
+
+    public void setDiscountPrice(int discountPrice) {
+        this.discountPrice = discountPrice;
+    }
 }
-class Book{
+
+class BookBuilder {
+    private Book book;
+
+    public BookBuilder() {
+        book = new Book();
+    }
+
+    public BookBuilder chooseBook(String name) {
+        book.setName(name);
+        book.setPrice(8);
+        return this;
+    }
+
+    public Book build() {
+        return book;
+    }
+}
+
+class Book {
     private String name;
+    private int price;
+
+    public Book() {
+    }
 
     public Book(String name, int price) {
         this.name = name;
@@ -46,7 +122,4 @@ class Book{
     public void setPrice(int price) {
         this.price = price;
     }
-
-    private int price;
-
 }
